dd18733c0c548da78b1a6882de60e333180c0673
robrpete <rob.r.pete@gmail.com>
2022-10-07 14:38:30 -0400

    format smite/models

--- capstone/smite/models.py
+++ capstone/smite/models.py
@@ -2,6 +2,8 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+
 class Skin(models.Model):
     god_icon_url = models.URLField()
     god_skin_url = models.URLField()
@@ -11,6 +13,7 @@ class Skin(models.Model):
     def __str__(self):
         return_str = self.god_name + self.god_skin_url
         return return_str
+
 
 class Session(models.Model):
     getter_id = models.IntegerField()
